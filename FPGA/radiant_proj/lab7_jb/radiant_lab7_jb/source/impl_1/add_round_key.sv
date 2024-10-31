8abcf08ef6e4de3e9dd05ec1ff3e2b422ed6699e
Jason <jbowman@hmc.edu>
2024-10-30 21:55:28 -0700

    first sim, ran in lab, added reset_int and sbox

--- FPGA/radiant_proj/lab7_jb/radiant_lab7_jb/source/impl_1/add_round_key.sv
+++ FPGA/radiant_proj/lab7_jb/radiant_lab7_jb/source/impl_1/add_round_key.sv
@@ -3,19 +3,3 @@
 // CREATED: 10-28-24
 // This module acts as the add round key part of AES, using XOR with the appropriate input depending on the round and throwing a flag once each round has been completed
 
-module add_round_key (
-	input logic [127:0] plaintext,
-	input logic [127:0] cypher_intermediate,
-	input logic [3:0] current_round,
-	input logic [127:0] current_round_key,
-	output logic [127:0] round_key_done);
-	
-	always_comb begin
-		case(current_round)
-			4'd1: round_key_done = plaintext ^ current_round_key;
-			4'd2, 4'd3, 4'd4, 4'd5, 4'd6, 4'd7, 4'd8, 4'd9, 4'd10: round_key_done = cypher_intermediate ^ current_round_key;
-			default: round_key_done = 128'd0;
-		endcase
-	end
-
-endmodule
